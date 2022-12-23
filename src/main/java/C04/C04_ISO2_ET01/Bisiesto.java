4e4581b35c099ead5f453b30961f0402b1ec3295
CristianRubio4 <114807919+CristianRubio4@users.noreply.github.com>
2022-12-23 14:19:10 +0100

    Add files via upload

--- src/main/java/C04/C04_ISO2_ET01/Bisiesto.java
+++ src/main/java/C04/C04_ISO2_ET01/Bisiesto.java
@@ -2,27 +2,26 @@ package C04._C04_ISO2_ET01_;
 
 public class Bisiesto {
 
-	private int dia;
-	private int mes;
-	private int anio;
+private int dia;
+private int mes;
+private int anio;
 
 	public Bisiesto(int dia, int mes, int anio) 
 			throws NumeroFueraDeRangoException {
 		if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anio < 0)
-			throw new
+			throw new 
 			NumeroFueraDeRangoException("Error al introducir los valores");
-		this.dia = dia;
-		this.mes = mes;
-		this.anio = anio;
+this.dia = dia;
+this.mes = mes;
+this.anio = anio;
 	}
 
 	public int getDia() {
 		return dia;
 	}
 
-	public void setDia(int dia)
-			throws NumeroFueraDeRangoException {
-		if (dia < 1 || dia > 31)
+	public void setDia(int dia) throws NumeroFueraDeRangoException {
+		if (dia < 1||dia > 31)
 			throw new 
 			NumeroFueraDeRangoException("Error al introducir los valores");
 
@@ -33,9 +32,8 @@ public class Bisiesto {
 		return mes;
 	}
 
-	public void setMes(int mes) 
-			throws NumeroFueraDeRangoException {
-		if (mes < 1 || mes > 12)
+	public void setMes(int mes) throws NumeroFueraDeRangoException {
+		if (mes < 1||mes > 12)
 			throw new
 			NumeroFueraDeRangoException("Error al introducir los valores");
 
@@ -46,18 +44,16 @@ public class Bisiesto {
 		return anio;
 	}
 
-	public void setAnio(int anio)
-			throws NumeroFueraDeRangoException {
+	public void setAnio(int anio) throws NumeroFueraDeRangoException {
 		if (anio < 0)
-			throw new
+			throw new 
 			NumeroFueraDeRangoException("Error al introducir los valores");
 
 		this.anio = anio;
 	}
 
-	public boolean bisiesto(int anio) 
-			throws NumeroFueraDeRangoException {
-		if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0))
+	public boolean bisiesto(int anio) throws NumeroFueraDeRangoException {
+		if (anio % 4 == 0 && (anio % 100!=0||anio % 400 == 0))
 			return true;
 
 		return false;
